21ed35fa23629734a8cc2b984bf8fd6a9a414b96
srscalia <shelby.scalia@gmail.com>
2018-08-23 14:13:51 -0400

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,10 +14,21 @@ def line(katz_deli)
   end
 end
 
+#def line(katz_deli)
+#  if katz_deli.length==0
+#    puts "The line is currently empty."
+#  else
+#    message= []
+#    katz_deli.each do |x|
+#      message<<" #{katz_deli.index+1}. #{x}"
+#    end
+#    puts "The line is currently:" + message.to_s
+#  end
+#end
+
 def take_a_number(katz_deli, name)
   katz_deli.push(name)
-  position = katz_deli.length
-  puts "Welcome, #{name}. You are number #{position} in line."
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
 def now_serving(katz_deli)
